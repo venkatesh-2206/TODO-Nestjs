7b9ec6be2e357c7c0b1b4d5a1b61f1d7d02d2837
venkatesh-2206 <seenuvasanvenkatesh@gmail.com>
2025-01-08 12:01:41 +0530

    added getuser

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,9 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete,UseGuards } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete,UseGuards, ParseIntPipe } from '@nestjs/common';
 import { UserService } from 'src/user/user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { JwtAuthGuard } from 'src/auth/guard/auth.guard';
+import { GetUser } from 'src/auth/decorator/auth.decorator';
+import { User } from './entities/user.entity';
 
 
 @Controller('users')
@@ -10,8 +12,8 @@ export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.userService.deleteUserById(+id);
+  remove(@GetUser() user:User ,@Param('id',ParseIntPipe) id: string) {
+    return this.userService.deleteUserById(id);
   }
 
 }
